87925d346fa018624e6d3bc334e39c9f4dfc0dca
Vrushant <vrushantmukherjee@gmail.com>
2024-07-30 00:49:19 +0530

    portfolio-creation-vd-19:20

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-function NavBar () {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <h1>VM</h1>
-        </li>
-      </ul>
-    </nav>
-  )
-}
-function buttonclicked () {
-  document.getElementsByClassName('h1')[0].innerHTML = 'Nothing Much You Tell'
-}
-function MyButton () {
-  return (
-    <button class='button1' onClick={buttonclicked}>
-      Submit
-    </button>
-  )
-}
-
+import Sidenav from './components/sidenav.jsx'
 function App () {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavBar />
-      <h1 class='h1'>Wassup Bro</h1>
-      <MyButton />
+      <Sidenav />
     </>
   )
 }
